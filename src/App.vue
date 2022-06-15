85b3de8dafa97cf7aa4de43267ac7c3943f0473d
Aurélien Lonni <aurelien.lonni@open-groupe.com>
2022-06-15 17:22:49 +0200

    Simple v-bind example
    Simple example of v-bind directive.

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <TextInterpolation message="Voici un message personnalisé réactif."/>
+    <SimpleVBind message="Voici un message personnalisé réactif."/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import TextInterpolation from './components/TextInterpolation.vue';
+import SimpleVBind from './components/SimpleVBind.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    TextInterpolation
+    TextInterpolation,
+    SimpleVBind
   }
 }
 </script>
---
+++ src/components/SimpleVBind.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="simple-v-bind" id="simple-v-bind">
+    <hr>
+    <h2>Liaison d'attributs avec la directive v-bind</h2>
+    <span v-bind:title="message">
+    Passer la souris sur ce texte pendant quelques secondes pour voir le titre dynamique.
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SimpleVBind',
+  props: {
+    message: String
+  }
+}
+
+</script>
+
+<style scoped>
+.simple-v-bind {
+  margin: 5em 0;
+}
+</style>
